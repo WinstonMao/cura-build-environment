6946e65052ba1c1ae9d0b9d1c8ec6a287909e582
Lipu Fei <lipu.fei815@gmail.com>
2018-03-26 13:20:12 +0200

    Update netifaces to 0.10.6

--- projects/python-netifaces.cmake
+++ projects/python-netifaces.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(PythonNetifaces
-    URL https://pypi.python.org/packages/18/fa/dd13d4910aea339c0bb87d2b3838d8fd923c11869b1f6e741dbd0ff3bc00/netifaces-0.10.4.tar.gz
-    URL_MD5 36da76e2cfadd24cc7510c2c0012eb1e
+    URL https://pypi.python.org/packages/72/01/ba076082628901bca750bf53b322a8ff10c1d757dc29196a8e6082711c9d/netifaces-0.10.6.tar.gz
+    URL_MD5 1d424cb5ef52907c5ab913011122a98b
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=netifaces-install.log
