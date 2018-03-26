3a0e7ed3fafd6889d7b97dc9058ad9a465d12e35
Lipu Fei <lipu.fei815@gmail.com>
2018-03-26 16:12:00 +0200

    Change Qt and PyQt to 5.10

--- projects/pyqt.cmake
+++ projects/pyqt.cmake
@@ -1,7 +1,7 @@
 set(pyqt_command "")
 if(BUILD_OS_WINDOWS)
     add_custom_target(PyQt
-        COMMAND ${PYTHON_EXECUTABLE} -m pip install PyQt5==5.10.1
+        COMMAND ${PYTHON_EXECUTABLE} -m pip install PyQt5==5.10.0
         COMMENT "Installing PyQt5"
     )
 
@@ -18,8 +18,8 @@ else()
     )
 
     ExternalProject_Add(PyQt
-        URL https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.10.1/PyQt5_gpl-5.10.1.tar.gz
-        URL_MD5 7e167b9617e7bd64012daaacb85477af
+        URL https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.10/PyQt5_gpl-5.10.tar.gz
+        URL_MD5 4874c5985246fdeb4c3c7843a3e6ef53
         CONFIGURE_COMMAND ${pyqt_command}
         BUILD_IN_SOURCE 1
     )
