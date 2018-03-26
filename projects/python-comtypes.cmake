e07621bd1ae0f05f52c4f7fb60c9a5b3db854888
Lipu Fei <lipu.fei815@gmail.com>
2018-03-26 13:24:59 +0200

    Update comtypes to 1.1.4

--- projects/python-comtypes.cmake
+++ projects/python-comtypes.cmake
@@ -1,7 +1,7 @@
 if(BUILD_OS_WINDOWS)
     ExternalProject_Add(PythonComtypes
-        URL https://pypi.python.org/packages/85/11/722b9ce6725bf8160bd8aca68b1e61bd9db422ab12dae28daa7defab2cdc/comtypes-1.1.3-2.zip
-        URL_MD5 4161cb8bc283a75af85e220ad662d5af
+        URL https://pypi.python.org/packages/df/79/2d3681f9a61b3dd09eb7774c1948a91a8cafaac6092a30370977a713c667/comtypes-1.1.4.zip
+        URL_MD5 a923a2e1d38e17b55e5edcdf0e04d12a
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
         INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=comtypes-install.log
