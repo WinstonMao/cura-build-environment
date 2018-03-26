8e72e080dfe4f5487f8ddacfe399eb9e8cca8531
Lipu Fei <lipu.fei815@gmail.com>
2018-03-26 13:10:19 +0200

    Update numpy-stl to 2.4.1

--- projects/numpy-stl.cmake
+++ projects/numpy-stl.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(NumpySTL
-    URL https://github.com/WoLpH/numpy-stl/archive/v2.2.3.tar.gz
-    URL_MD5 fbd2abc07fdbbb302a649f940f2ae4cc
+    URL https://github.com/WoLpH/numpy-stl/releases/download/v2.4.1/numpy-stl-v2.4.1.tar.xz
+    URL_MD5 d0282e04d5715df83b5edd499aedb951
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
     INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=numpystl-install.log
