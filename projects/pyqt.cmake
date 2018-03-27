50c44a4d5fede7449ad054f1aeefb5863d975c72
Lipu Fei <lipu.fei815@gmail.com>
2018-03-27 15:17:01 +0200

    WIP: Compile Qt on Windows

--- projects/pyqt.cmake
+++ projects/pyqt.cmake
@@ -1,21 +1,19 @@
 set(pyqt_command "")
-    set(pyqt_command
-        # On Linux, PyQt configure fails because it creates an executable that does not respect RPATH
-        "LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib"
-        ${PYTHON_EXECUTABLE} configure.py
-        --sysroot ${CMAKE_INSTALL_PREFIX}
-        --qmake ${CMAKE_INSTALL_PREFIX}/bin/qmake
-        --sip ${CMAKE_INSTALL_PREFIX}/bin/sip
-        --confirm-license
-    )
+set(pyqt_command
+    # On Linux, PyQt configure fails because it creates an executable that does not respect RPATH
+    "LD_LIBRARY_PATH=${CMAKE_INSTALL_PREFIX}/lib"
+    ${PYTHON_EXECUTABLE} configure.py
+    --sysroot ${CMAKE_INSTALL_PREFIX}
+    --qmake ${CMAKE_INSTALL_PREFIX}/bin/qmake
+    --sip ${CMAKE_INSTALL_PREFIX}/bin/sip
+    --confirm-license
+)
 
-    ExternalProject_Add(PyQt
-        URL https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.10/PyQt5_gpl-5.10.tar.gz
-        URL_MD5 4874c5985246fdeb4c3c7843a3e6ef53
-        CONFIGURE_COMMAND ${pyqt_command}
-        BUILD_IN_SOURCE 1
-    )
+ExternalProject_Add(PyQt
+    URL https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.10/PyQt5_gpl-5.10.tar.gz
+    URL_MD5 4874c5985246fdeb4c3c7843a3e6ef53
+    CONFIGURE_COMMAND ${pyqt_command}
+    BUILD_IN_SOURCE 1
+)
 
-    SetProjectDependencies(TARGET PyQt DEPENDS Qt Sip)
-
-endif()
+SetProjectDependencies(TARGET PyQt DEPENDS Qt Sip)
