e0ad3270a03cfea5b5f266e70dc7f596c5c554aa
Lipu Fei <lipu.fei815@gmail.com>
2018-03-29 12:43:33 +0200

    Add --prefix for pip

--- projects/python-pip.cmake
+++ projects/python-pip.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(PythonPip
     URL https://pypi.python.org/packages/c4/44/e6b8056b6c8f2bfd1445cc9990f478930d8e3459e9dbf5b8e2d2922d64d3/pip-9.0.3.tar.gz
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${PYTHON_EXECUTABLE} setup.py build
-    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --single-version-externally-managed --record=pip-install.log
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} setup.py install --prefix ${CMAKE_INSTALL_PREFIX} --single-version-externally-managed --record=pip-install.log
     BUILD_IN_SOURCE 1
 )
 SetProjectDependencies(TARGET PythonPip DEPENDS Python)
